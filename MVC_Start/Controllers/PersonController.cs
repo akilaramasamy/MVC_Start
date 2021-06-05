117363878b123b407f1213e3049b1bb534c802b3
Akila Ramasamy <akilaramasamy@usf.edu>
2021-06-05 15:01:46 -0400

    In-class Assignment : Razor form

---
+++ MVC_Start/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_Start.Models;
+
+namespace MVC_Start.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            PersonViewModel person = new PersonViewModel
+            {
+                FirstName = "Akila",
+                LastName = "Ramasamy",
+                Age = 100,
+                Email = "aramasamy@usf.edu",
+                Phone = "12345",
+                Address = "756  jgyf"
+
+            };
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult Save(PersonViewModel model)
+        {
+            return View();
+        }
+     }
+}
---
+++ MVC_Start/Models/PersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Start.Models
+{
+    public class PersonViewModel
+    {
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
